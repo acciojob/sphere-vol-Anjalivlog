21bb194e59c881d3862f2aa9a1f4e152040960ca
Anjalivlog <139326744+Anjalivlog@users.noreply.github.com>
2024-04-03 16:49:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-function volume_sphere() {
+function volume_sphere(event) {
+    event.preventDefault();
     //Write your code here
-	const radiusInput = document.getElementById('radius').value;
-	const radius = parseFloat(radiusInput);
-	if (isNaN(radius) || radius < 0) {
-		document.getElementById('volume').value = 'NaN';
-	} else {
-		const volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
-		document.getElementById('volume').value = volume.toFixed(4);
-	}
+    const radiusInput = document.getElementById('radius').value;
+    const radius = parseFloat(radiusInput);
+    if (isNaN(radius) || radius < 0) {
+        document.getElementById('volume').value = 'NaN';
+    } else {
+        const volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
+        document.getElementById('volume').value = volume.toFixed(4);
+    }
 } 
 
-// window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
 window.onload = function() {
     document.getElementById('MyForm').onsubmit = volume_sphere;
 }
