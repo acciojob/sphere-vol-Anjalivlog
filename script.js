6d358df171da992a6058a0a04d9de2b1348f6b82
Anjalivlog <139326744+Anjalivlog@users.noreply.github.com>
2024-04-03 16:41:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ function volume_sphere() {
 	const radiusInput = document.getElementById('radius').value;
 	const radius = parseFloat(radiusInput);
 	if (isNaN(radius) || radius < 0) {
-		document.getElementById('volume').value = 'NaN';
+		document.getElementById('#volume').value = 'NaN';
 	} else {
 		const volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
-		document.getElementById('volume').value = volume.toFixed(4);
+		document.getElementById('#volume').value = volume.toFixed(4);
 	}
 } 
 
