ba6bcab0c95d0a5688ead284aaf6620f4151261f
Anjalivlog <139326744+Anjalivlog@users.noreply.github.com>
2024-04-03 16:14:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 function volume_sphere() {
     //Write your code here
-  
+	const radiusInput = document.getElementById('radius').value;
+	const radius = parseFloat(radiusInput);
+	if (!NaN(radius) || radius < 0) {
+		document.getElementById('volume').value = 'NaN';
+	} else {
+		const volumn = (4 / 3) * Math.PI * Math.pow(radius, 3);
+		document.getElementById('volume').value = volume.toFixed(4);
+	}
+  return false;
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
